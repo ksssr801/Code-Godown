4e682a26cad715c665cc77a1eec2483ca1b9ccff
krsahas <krsahas.git@gmail.com>
2020-05-10 01:01:24 +0530

    some fixes for make_dir.py

--- source/make_dir.py
+++ source/make_dir.py
@@ -9,13 +9,13 @@ make_config_file = os.path.join(file_path, 'config')
 
 # writing credentials file
 cred_file = open(make_cred_file, 'w')
-to_cred_file = '[default]\naws_access_key_id = AKIAJN7ZHSRXBAHD4DMQ\naws_secret_access_key = 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'
+to_cred_file = '[default]\naws_access_key_id = access_key\naws_secret_access_key = secret_key'
 cred_file.write(to_cred_file)
 cred_file.close()
 
 # writing config file
 config_file = open(make_config_file, 'w')
-to_config_file = '[default]\noutput = json\nregion = ap-south-1'
+to_config_file = '[default]\noutput = json\nregion = region'
 config_file.write(to_config_file)
 config_file.close()
 
