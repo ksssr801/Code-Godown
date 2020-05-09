a0d1ba26cafe449645a506082feb5a7cf7f733b7
krsahas <krsahas.git@gmail.com>
2020-05-10 01:03:24 +0530

    fixes to make_backup.sh

--- source/make_backup.sh
+++ source/make_backup.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-SRC_DIR="/opt/Everest"
-DEST_DIR="/opt/everest_backup/eve_backup"
+SRC_DIR="path/to/source"
+DEST_DIR="path/to/destination"
 FILENAME=_$(date +%-Y%-b%-d)-$(date +%-T).tgz
 tar --create --gzip --file=$DEST_DIR$FILENAME $SRC_DIR
-
-
-# SRC_DIR="/opt/Everest/otn"
-# DEST_DIR="/opt/otn_backup/otn_backup"
-# FILENAME=_$(date +%-Y%-b%-d)-$(date +%-T).tgz
-# tar --create --gzip --file=$DEST_DIR$FILENAME $SRC_DIR
 
 
 # SRC_DIR="/Sattu/coding"
